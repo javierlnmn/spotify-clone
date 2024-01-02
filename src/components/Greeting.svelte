19d38a56c59fdb0f3c032bbc740c3816c3439ea7
javierlnmn <jlm03123@gmail.com>
2024-01-02 11:50:14 +0100

    Svelte component for greetings

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    let greeting = '';
+    if (currentHour < 12 ) {
+        greeting = 'Good morning!';
+    } else if (currentHour < 18 ) {
+        greeting = 'Good afternoon!';
+    } else {
+        greeting = 'Good evening!';
+    }
+</script>
+
+<h3 class="font-bold text-4xl">{greeting}</h3>
