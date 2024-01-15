e2484b1bc2834319afbc05f34be0cd9acbd51944
javierlnmn <jlm03123@gmail.com>
2024-01-15 10:44:44 +0100

    Volume set to state and made component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from "@/store/playerStore";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Slider } from "./Slider";
 
 export const Pause = ({ className }) => (
@@ -18,6 +18,29 @@ export const Volume = () => (
     <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
 )
 
+const VolumeControl = () => {
+
+    const volume = usePlayerStore(state => state.volume);
+    const setVolume = usePlayerStore(state => state.setVolume);
+
+    return (
+        <div className="flex items-center justify-center gap-2">
+            { volume < 0.01 ? <VolumeSilence /> : <Volume />}
+            <Slider 
+                defaultValue={[100]}
+                max={100}
+                min={0}
+                className="w-[95px] bg-green-50"
+                onValueChange={(value) => {
+                    const [newVolume] = value;
+                    const volumeValue = newVolume / 100;
+                    setVolume(volumeValue)
+                }}
+            />
+        </div>
+    )
+}
+
 const CurrentSong = ({ image, title, artists }) => {
     return (
         <div className="flex items-center gap-5 relative overflow-hidden">
@@ -34,7 +57,7 @@ const CurrentSong = ({ image, title, artists }) => {
 
 export function Player() {
 
-    const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state);
+    const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state);
     
     const audioRef = useRef();
 
@@ -50,10 +73,15 @@ export function Player() {
         if(song) {
             const src = `/music/${playlist.id}/0${song.id}.mp3`;
             audioRef.current.src = src;
+            audioRef.current.volume = volume;
             audioRef.current.play();
         }
 
     }, [currentMusic]);
+
+    useEffect(() => {
+        audioRef.current.volume = volume;
+    }, [volume]);
 
     const handleClick = () => {
         setIsPlaying(!isPlaying);
@@ -80,16 +108,7 @@ export function Player() {
             </div>
 
             <div className="grid place-content-center">
-                <Slider 
-                    defaultValue={[100]}
-                    max={100}
-                    min={0}
-                    className="w-[95px] bg-green-50"
-                    onValueChange={(value) => {
-                        const [newVolume] = value;
-                        audioRef.current.volume = newVolume / 100;
-                    }}
-                />
+                <VolumeControl />
             </div>
 
         </div>
